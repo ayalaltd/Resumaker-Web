d614d763cc22763a72f3baec632aa4b90f405959
Mohamad Saleh <mohsley@outlook.com>
2021-08-02 12:22:52 -0700

    Working on linking front-end to back-end

--- src/main/java/com/resumaker/resumakerweb/ResuMakerWebApplication.java
+++ src/main/java/com/resumaker/resumakerweb/ResuMakerWebApplication.java
@@ -2,12 +2,14 @@ package com.resumaker.resumakerweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import com.resumaker.resumakerweb.SubmitController;
 @SpringBootApplication
 public class ResuMakerWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ResuMakerWebApplication.class, args);
+        String [] info = {"Mohamad Saleh", "mohs@outlook.com", "707-414-2323"};
+        SubmitController.contactSubmit(info);
     }
 
 }
--- src/main/java/com/resumaker/resumakerweb/SubmitController.java
+++ src/main/java/com/resumaker/resumakerweb/SubmitController.java
@@ -5,31 +5,33 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.*;
 
-@Controller
+//@Controller
 public class SubmitController {
     static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
     static final String USER = "program";
     static final String PASS = "1234";
 
-    @PostMapping("/submit")
-    public String submit(@RequestParam String[] contactPOST,
-                         @RequestParam String[] skillPOST,
-                         @RequestParam String[] workPOST,
-                         @RequestParam String[] educationPOST) {
+//    @PostMapping("/submit")
+//    public void submit(@RequestParam String[] contactPOST, @RequestParam String[] skillPOST, @RequestParam String[] workPOST, @RequestParam String[] educationPOST) {
+//        contactSubmit(contactPOST);
+//    }
 
-    }
-
-    static void contactSubmit(String[] contactPOST) {
+    public static void contactSubmit(String[] contactPOST) {
         String values = "("+ contactPOST[0] + ", " + contactPOST[1] + ", " + contactPOST[2] + ")";
 
         try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
             Statement stmt = conn.createStatement();
-            stmt.executeQuery("INSERT INTO public.\"Contact\"(\n" +
+            ) {
+            String sql = "INSERT INTO public.\"Contact\"(\n" +
                     "\tname, email, phone)\n" +
-                    "\tVALUES" + values + ";");) {
+                    "\tVALUES" + values + ";";
+            stmt.execute(sql);
         } catch (SQLException e) {
             e.printStackTrace();
         }
